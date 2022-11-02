acfec3b7d2312ae24876d767819186b6bcea398e
Kfried <phil.bridge@ixapps.co.uk>
2022-11-02 08:52:24 +0000

    string challenges

---
+++ StringExercise.py
@@ -0,0 +1,27 @@
+# check one
+# given a string create a new string from the first middle and last characters
+test_string = "Three"
+
+
+# check two
+# given a string create a new string made up of the three middle characters
+test_string = "IsolationLetCompation"
+test_string2 = "Lip"
+
+
+
+# check three
+# given two strings make a new string from the first middle and last characters of each
+test_string = "England"
+test_string2 = "Wales"
+
+
+# check four
+# given a string how many letter p is is it (case sensitive)
+test_string = "Penelope played aptly opposing Peter"
+
+# modify for case insesnitive
+
+# check five
+# palindrome
+# write code to determine if a string is a palindrome
